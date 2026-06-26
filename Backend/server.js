beb7907dc9dfd21b09d717c3a87c5ba19b266e83
Srijan Ponaganti <sp23eeb0a26@student.nitw.ac.in>
2026-06-26 20:26:11 +0530

    deleted node modules

--- Backend/server.js
+++ Backend/server.js
@@ -2,13 +2,13 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '.env') });
 const server = require('./src/app.js');
 const connectDB = require('./src/db/db.js');
+const PORT = process.env.PORT || 3000;
 
 (async () => {
     try {
         await connectDB();
-        server.listen(3000, () => {
-            console.log("Server is running on port 3000");
-        });
+        server.listen(PORT, () => 
+            console.log(`Server running on port ${PORT}`));
     } catch (error) {
         console.error("Failed to start server:", error);
         process.exit(1);
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173', // The exact URL of your Vite React app
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173', // The exact URL of your Vite React app
     credentials: true                // Required to allow cookies to pass through
 }));
 
